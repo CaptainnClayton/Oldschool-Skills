8f5761862afbd50fdcca5e568fb6ee2d31ad338e
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-08 19:51:29 -0700

    Set up Storyboard and View Controllers

---
+++ RuneScapeSkills/RuneScapeSkills/Models/SkillTemplate.swift
@@ -0,0 +1,15 @@
+//
+//  SkillTemplate.swift
+//  RuneScapeSkills
+//
+//  Created by Clayton Watkins on 4/8/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+struct Skill {
+    var name: String
+    var style: String
+    var definition: String
+}
---
+++ RuneScapeSkills/RuneScapeSkills/ViewControllers/SkillsTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SkillsTableViewController.swift
+//  RuneScapeSkills
+//
+//  Created by Clayton Watkins on 4/8/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class SkillsTableViewController: UITableViewController {
+
+
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
